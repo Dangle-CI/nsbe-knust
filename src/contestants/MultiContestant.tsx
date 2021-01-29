77874a7a1dd2ce80e991c501fe3995334a12dcac
Emmanuel Opoku Hinneh <hinneh04@gmail.com>
2021-01-29 10:42:43 +0000

    edits

--- src/contestants/MultiContestant.tsx
+++ src/contestants/MultiContestant.tsx
@@ -1,44 +1,45 @@
-import { Card, CardActionArea, CardContent, Grid } from '@material-ui/core';
+import {Card, CardActionArea, CardContent, Grid} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
-import React, { useState } from 'react';
-import pic from '../images/enoch.jpg';
+import React, {useState} from 'react';
 import './cont_index.css';
+import {BASE_URL} from "../contestant_sample_data";
 
 export default function MultiContestant(props) {
-    const [selected,setSelected] = useState('')
-    const {contestants,selectionHandler,portfolio} = props
-   
+    const [selected, setSelected] = useState('')
+    const {contestants, selectionHandler, portfolio} = props
+
     return (
-       <>
-       {
-           contestants.map((contestant)=>{
-            const {memberDetails} = contestant
-            let name = memberDetails.surname + " "+ memberDetails.otherNames
-            let id = memberDetails.memberId
-                return  <Grid item  key={contestant.name} xs={6} sm={3}  className='grid-items'>
-                <div className="contestant-container" onClick={()=>{
-                    setSelected(id)
-                    selectionHandler(contestant, portfolio,true)
-                    }}>
-                    <div className="down-layer">
-                        <Card className="contestant">
-                            <CardActionArea>
-                                <img className='contestant-img' src={pic} />
-                            </CardActionArea>
-                            <CardContent className='card-content'>
-                                <Typography className='card-title'>{name}</Typography>
-                            </CardContent>
+        <>
+            {
+                contestants.map((contestant) => {
+                    const {memberDetails} = contestant
+                    let name = memberDetails.surname + " " + memberDetails.otherNames
+                    let id = memberDetails.memberId
+                    return <Grid item key={contestant.name} xs={6} sm={3} className='grid-items'>
+                        <div className="contestant-container" onClick={() => {
+                            setSelected(id)
+                            selectionHandler(contestant, portfolio, true)
+                        }}>
+                            <div className="down-layer">
+                                <Card className="contestant">
+                                    <CardActionArea>
+                                        <img className='contestant-img'
+                                             src={`${BASE_URL}/election/aspirant/${contestant.image}`}/>
+                                    </CardActionArea>
+                                    <CardContent className='card-content'>
+                                        <Typography className='card-title'>{name}</Typography>
+                                    </CardContent>
 
-                        </Card>
-                    </div>
-                    <div className={selected === id ? 'top-layer' : ''}>
+                                </Card>
+                            </div>
+                            <div className={selected === id ? 'top-layer' : ''}>
 
-                    </div>
-                </div>
-            </Grid>
-           })
-       }
-       
-       </>
+                            </div>
+                        </div>
+                    </Grid>
+                })
+            }
+
+        </>
     )
 }
--- src/contestants/SingleContestant.tsx
+++ src/contestants/SingleContestant.tsx
@@ -1,56 +1,57 @@
-import { Button, Card, CardActionArea, CardContent } from '@material-ui/core';
+import {Button, Card, CardActionArea, CardContent} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
-import { Check, Close } from '@material-ui/icons';
-import React, { useEffect, useState } from 'react';
-import pic from '../images/enoch.jpg';
+import {Check, Close} from '@material-ui/icons';
+import React, {useState} from 'react';
+import {BASE_URL} from "../contestant_sample_data";
 
 export default function SingleContestant(props) {
-    const [selection,setSelection] = useState(null)
-    
+    const [selection, setSelection] = useState(null)
 
-    const {contestants,selectionHandler,portfolio} = props
+
+    const {contestants, selectionHandler, portfolio} = props
     const {memberDetails} = contestants
-  
+
     return (
-       <>
-       <div className="single-contestant">
-       <div className="contestant-container inlined-items">
-                            <div className="down-layer">
-                                <Card className="contestant">
-                                    <CardActionArea>
-                                        <img className='contestant-img' src={pic} />
-                                    </CardActionArea>
-                                    <CardContent className='card-content'>
-                                        <Typography className='card-title'>
-                                            {memberDetails.surname+ " " +memberDetails.otherNames}
-                                        </Typography>
-                                    </CardContent>
+        <>
+            <div className="single-contestant">
+                <div className="contestant-container inlined-items">
+                    <div className="down-layer">
+                        <Card className="contestant">
+                            <CardActionArea>
+                                <img className='contestant-img'
+                                     src={`${BASE_URL}/election/aspirant/${contestants.image}`}/>
+                            </CardActionArea>
+                            <CardContent className='card-content'>
+                                <Typography className='card-title'>
+                                    {memberDetails.surname + " " + memberDetails.otherNames}
+                                </Typography>
+                            </CardContent>
 
-                                </Card>
-                            </div>
-                            <div className="top-layer">
+                        </Card>
+                    </div>
+                    <div className="top-layer">
 
-                            </div>
-                        </div>
-                        <div className='inline-items'>
-                        <Button className={selection === 'yes' ? 'yes-button' : 'raw-btn'} 
-                        onClick={()=>{
-                            setSelection('yes')
-                            selectionHandler(contestants,portfolio,true)
-                        }}
-                        startIcon={<Check/>}
-                        variant='contained' color='primary' >
-                            Yes</Button> 
-                        <Button className={selection === 'no' ? 'yes-button' : 'raw-btn'}   color='primary'
-                        onClick={()=>{
-                            setSelection('no')
-                            selectionHandler(contestants,portfolio,true)
-                        }}
-                        startIcon={<Close/>}
-                        variant='contained' >No</Button>
-                        </div>
-       </div>
-       
-       </>
+                    </div>
+                </div>
+                <div className='inline-items'>
+                    <Button className={selection === 'yes' ? 'yes-button' : 'raw-btn'}
+                            onClick={() => {
+                                setSelection('yes')
+                                selectionHandler(contestants, portfolio, true)
+                            }}
+                            startIcon={<Check/>}
+                            variant='contained' color='primary'>
+                        Yes</Button>
+                    <Button className={selection === 'no' ? 'yes-button' : 'raw-btn'} color='primary'
+                            onClick={() => {
+                                setSelection('no')
+                                selectionHandler(contestants, portfolio, true)
+                            }}
+                            startIcon={<Close/>}
+                            variant='contained'>No</Button>
+                </div>
+            </div>
+
+        </>
     )
 }
